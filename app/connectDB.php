2760431943237ea9d7a27fc926b40611bea82229
Doeun Kim <graceee318@gmail.com>
2018-05-23 14:50:27 +0900

    Delete connectDB.php

--- app/connectDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hn = 'localhost';
-    $db = 'fpdb';
-    $un = 'root';
-    $pw = 'root';
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if ($conn->connect_error) die($conn->connect_error);
-?>
