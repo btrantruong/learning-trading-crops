b3c8aca776c7ea45bdc9edf2a4f9cdebb19e2b25
Doeun Kim <graceee318@gmail.com>
2018-05-26 00:13:43 +0900

    create farmerMain and buyerMain

---
+++ view/farmerMain.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Greatest Farmer</title>
+    <meta name="description" content="Free Bootstrap Theme by uicookies.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+    <link rel="stylesheet" href="../css/styles-merged.css">
+    <link rel="stylesheet" href="../css/style.min.css">
+    <link rel="stylesheet" href="../css/custom.css">
+
+  </head>
+  <body>
+
+  <!-- START: header -->
+  
+  <div class="probootstrap-loader"></div>
+
+  <header role="banner" class="probootstrap-header">
+    <div class="container">
+        <a href="index.html" class="probootstrap-logo" style="margin-right:20px;">The Greatest Farmer<span>.</span></a>
+        <a href="" style="margin-right: 10px;color:green;">Learn</a>
+        <a href="" style="margin-right: 10px;color:green;">Teach</a>
+        <a href="" style="margin-right: 10px;color:green;">Sell</a>
+        <a href="" style="margin-right: 10px;color:green;">Subscribe</a>
+        <nav role="navigation" class="probootstrap-nav hidden-xs">
+          <ul class="probootstrap-main-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <div class="btn-group">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Hello, <span>Doeun</span>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <a href="" style="padding-left:10px;">My Product</a><br>
+                    <a href="" style="padding-left:10px;">My Subscription</a><br>
+                    <a href="" style="padding-left:10px;">My Written Post</a><br>
+                    <a href="" style="padding-left:10px;">My Saved Post</a>
+                </div>
+            </div>
+            <li><a href="">Logout</a></li>
+          </ul>
+        </nav>
+    </div>
+  </header>
+  <!-- END: header -->
+  
+  <div class="probootstrap-section">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 mb40">
+          <h2></h2>       
+        </div>
+      </div>
+
+      
+    </div>
+  </div>
+
+
+  <div class="gototop js-top">
+    <a href="#" class="js-gotop"><i class="icon-chevron-thin-up"></i></a>
+  </div>
+  
+
+  <script src="../js/scripts.min.js"></script>
+  <script src="../js/main.min.js"></script>
+  <script src="../js/custom.js"></script>
+
+  </body>
+</html>
